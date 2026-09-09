3d5507f11cb17e630807b686d8893a2962b4c694
royaboya <94084201+royaboya@users.noreply.github.com>
2026-09-09 17:43:41 -0400

    build again.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://royaboya.github.io",
-  // base: /portfolio
-  //base: "royaboya.github.io",
+  base: "/"
 });
