335c740c08d38fc901aa142ad7ef9fa3ace328ec
royaboya <94084201+royaboya@users.noreply.github.com>
2026-09-09 17:30:33 -0400

    rebuild, base misconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,5 +14,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://royaboya.github.io",
-  base: "/portfolio",
+  // base: /portfolio
+  base: "royaboya.github.io",
 });
